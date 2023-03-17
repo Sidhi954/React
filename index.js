38637b66a9a1d249056bd231b89c95837c6be8ca
Sidhi954 <79316354+Sidhi954@users.noreply.github.com>
2023-03-17 19:09:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <>
+      <h1>Netflix Top 5 Series</h1>
+      <p>List is below:</p>
+      <ol>
+        <li>Sex Education (2019)</li>
+        <li>The Baby-Sitters Club (2020)</li>
+        <li>Sweet Tooth (2021)</li>
+        <li>Breaking Bad (2008)</li>
+        <li>The Queen's Gambit (2020) </li>
+      </ol>
+  </>,
+document.getElementById('root'));
