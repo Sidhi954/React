e9f7cfafa97e7f256ea7303d418cf0b5280bd699
Sidhi954 <79316354+Sidhi954@users.noreply.github.com>
2023-03-17 19:31:41 +0530

    Add files via upload
    JAVASCRIPT EXPRESSION IN JSX

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//Small Project that defines Fragments
-ReactDOM.render(
-  <>
-      <h1>Netflix Top 5 Series</h1>
-      <p>List is below:</p>
-      <ol>
-        <li>Sex Education (2019)</li>
-        <li>The Baby-Sitters Club (2020)</li>
-        <li>Sweet Tooth (2021)</li>
-        <li>Breaking Bad (2008)</li>
-        <li>The Queen's Gambit (2020) </li>
-      </ol>
-  </>,
-document.getElementById('root'));
+const flname="Sidhi Raj";
+
+ReactDOM.render(<><h1>My name is {flname}</h1>
+<p>My roll number is {5+3}</p>
+</>,
+document.getElementById("root"));
