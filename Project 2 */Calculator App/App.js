5a9292fe7b16dbfa86ab4de405f858c5586d0643
Sidhi954 <79316354+Sidhi954@users.noreply.github.com>
2023-03-19 15:29:39 +0530

    Add files via upload

---
+++ Project 2 */Calculator App/App.js
@@ -0,0 +1,17 @@
+
+
+ 
+function add(num1,num2){
+  return (num1+num2);
+}
+function sub(num1,num2){
+  return(num1-num2);
+}
+function mul(num1,num2){
+  return(num1*num2);
+}
+function div(num1,num2){
+  return((num1/num2).toFixed(2));
+}
+export default add;
+export {sub,mul,div}
