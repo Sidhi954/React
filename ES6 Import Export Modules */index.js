21dcf8757e8de672c95d6404329860468df88a41
Sidhi954 <79316354+Sidhi954@users.noreply.github.com>
2023-03-19 12:10:31 +0530

    Add files via upload

---
+++ ES6 Import Export Modules */index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import myfav,{favprog,myName }from './App';
+
+ReactDOM.render(<>
+
+<ol>
+  <li>Sidhi</li>
+  <li>{myfav}</li>
+  <li>{favprog}</li>
+  <li>{myName()}</li>
+</ol>
+</>,document.getElementById("root"));
