72607dddd10ac0839cfea042077e7335734d5cae
Sidhi954 <79316354+Sidhi954@users.noreply.github.com>
2023-03-19 11:23:58 +0530

    Add files via upload

---
+++ Components in React/List.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function List(){
+    return(
+        <ol>
+            <li>Fun</li>
+            <li>Enjoy</li>
+            <li>Laugh</li>
+            <li>Memories</li>
+            <li>Excitement</li>
+        </ol>
+    );
+}
+
+export default List;
