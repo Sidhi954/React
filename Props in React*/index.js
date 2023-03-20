96cfe1c116da5aa36b3f975d808a11993997dabf
Sidhi954 <79316354+Sidhi954@users.noreply.github.com>
2023-03-20 11:30:30 +0530

    Create index.js

---
+++ Props in React*/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from './Card';
+
+
+ReactDOM.render(
+  <>
+    <Card imgsrc="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS15WXzJnh5FKZcjV7jwxj57HtPiu-mJyMBPJL7E2rPEFbLRPjA"
+      title="A Netflix Original Series "
+      sname='DARK'
+      link="https://www.netflix.com/in/title/80100172"
+    />
+    <Card imgsrc="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS15WXzJnh5FKZcjV7jwxj57HtPiu-mJyMBPJL7E2rPEFbLRPjA"
+      title="A Netflix Original Series "
+      sname='DARK'
+      link="https://www.netflix.com/in/title/80100172"
+    />
+  </>,
+  document.getElementById("root")
+);
