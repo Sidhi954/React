d6b233066d2ca6e07294c1fef95026f41ffa44fd
Sidhi954 <79316354+Sidhi954@users.noreply.github.com>
2023-03-21 11:10:37 +0530

    Add files via upload

---
+++ Arrays in React*/Sdata.jsx
@@ -0,0 +1,19 @@
+const Sdata=[
+    {
+        sname:'DARK',
+        imgsrc:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS15WXzJnh5FKZcjV7jwxj57HtPiu-mJyMBPJL7E2rPEFbLRPjA",
+        title:"A Netflix Original Series ",
+        link:"https://www.netflix.com/in/title/80100172",
+    },{
+        sname:'PERFECT MATCH',
+        imgsrc:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRVwvbJf_nalNn6JsGKKw5gzw0wcJzufXI5GYI_HWSp9L3pQUoJ",
+        title:"A Netflix Original Series ",
+        link:"https://www.netflix.com/in/title/81497817",
+    },{
+        sname:'YOU',
+        imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVR-ltLfb9oO71KHr4crOgtc9zh7JeaiORZSOl4zUg&usqp=CAE&s",
+        title:"A Netflix Original Series ",
+        link:"https://www.netflix.com/in/title/80211991",
+    }
+];
+export default Sdata;
