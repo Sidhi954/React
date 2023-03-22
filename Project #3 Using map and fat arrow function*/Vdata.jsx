5d1b927f5b543f16e2469142460887e208b27b69
Sidhi954 <79316354+Sidhi954@users.noreply.github.com>
2023-03-22 11:44:49 +0530

    Add files via upload

---
+++ Project #3 Using map and fat arrow function*/Vdata.jsx
@@ -0,0 +1,38 @@
+const Vdata=[{
+    imgsrc:"https://prod-discovery.edx-cdn.org/media/course/image/72abaeb3-0856-4e89-906a-8b6eef402a1f-153a590f7f3c.small.jpg",
+    sname:"INTRODUCTION TO C++",
+    title:"Data Structures and Algorithms",
+    link:"https://www.youtube.com/embed/z9bZufPHFLU",
+},{
+    imgsrc:"https://www.softwaretestinghelp.com/wp-content/qa/uploads/2019/06/autumn-lookbook.png",
+    sname:"Data Types & Type Modifiers",
+    title:"Data Structures and Algorithms",
+    link:"https://www.youtube.com/embed/cnT1oW5_ePM",
+},{
+    imgsrc:"https://www.softwaretestinghelp.com/wp-content/qa/uploads/2019/05/File-Input-Output-in-C.png",
+    sname:"INPUT/OUTPUT in C++",
+    title:"Data Structures and Algorithms",
+    link:"https://www.youtube.com/embed/7dPdMtBX1d8",
+},{
+    imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREFhh8Zzvlg7SC9ZYCrEHkuZw3Wnbf7o5JgA&usqp=CAU",
+    sname:"If/else Statement in C++",
+    title:"Data Structures and Algorithms",
+    link:"https://www.youtube.com/embed/cyB3HNlQyjY",
+},{
+    imgsrc:"https://i.ytimg.com/vi/Y0TYYEhHvIU/hqdefault.jpg",
+    sname:"Introduction to Loops",
+    title:"Data Structures and Algorithms",
+    link:"https://www.youtube.com/embed/IlIn_D1RT7M",
+},{
+    imgsrc:"https://cdn.educba.com/academy/wp-content/uploads/2019/12/Break-vs-Continue-2.jpg",
+    sname:"Break and Continue Statement",
+    title:"Data Structures and Algorithms",
+    link:"https://www.youtube.com/embed/Stf7KBiA1vs",
+},{
+    imgsrc:"https://cdn.educba.com/academy/wp-content/uploads/2019/09/Switch-Statement-in-C.png",
+    sname:"Switch-Case Statement",
+    title:"Data Structures and Algorithms",
+    link:"https://www.youtube.com/embed/G80-j_xnE_8",
+}];
+
+export default Vdata;
