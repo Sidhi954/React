0ccb8c18350c1db4588af3212b973fb3ffa80c4b
Sidhi954 <79316354+Sidhi954@users.noreply.github.com>
2023-04-01 23:09:18 +0530

    Add files via upload

---
+++ Project #4 using props*/Vdata.jsx
@@ -0,0 +1,28 @@
+const Vdata=[{
+        imgsrc:"https://5.imimg.com/data5/KI/FM/YD/SELLER-42297160/men-formal-shoes-500x500.jpeg",
+        title:"bacca bucci",
+        head:"Men Black Colourblocked Sneakers",
+        price:"₹1499",
+},{
+    imgsrc:"https://cdn.shopify.com/s/files/1/0607/6678/1671/products/CAMPDENVER_WHT-L.GRY.jpg?v=1670325503",
+    title:"Roadster",
+    head:"White Solid Casual Shoes For Men",
+    price:"₹474",
+},{
+    imgsrc:"https://economictimes.indiatimes.com/thumb/height-450,width-600,imgsize-77978,msid-95893756/skx52942_shopbystylegridupdate_men_750x664_casual_sneakers.jpg?from=mdr",
+    title:"Zixer ",
+    head:"Korean Style High Top Platform Fashion Sneakers/Sports/Casual Shoes",
+    price:"₹690",
+},{
+    imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAZe1MfWV2_2mM6cuiaQvyiB3XGfCAnmIrDw&usqp=CAU",
+    title:"Zixer",
+    head:"Formal Shoes for Men Leather Look Slip On For Men  (Black)",
+    price:"₹641",
+},{
+    imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0U8iTmIkP37r9o3rZEY9c5k9ZYO4ue9NuUA&usqp=CAU",
+    title:"JACK & JONES",
+    head:"WHITE SNEAKERS",
+    price:"₹4,499",
+}];
+
+export default Vdata;
