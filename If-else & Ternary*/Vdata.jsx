9c5bea741c4dbdf77fc82f597f9a774851375ee0
Sidhi954 <79316354+Sidhi954@users.noreply.github.com>
2023-04-09 20:25:36 +0530

    Add files via upload

---
+++ If-else & Ternary*/Vdata.jsx
@@ -0,0 +1,45 @@
+const Vdata=[{
+    id:1,
+    imgsrc:"https://prod-discovery.edx-cdn.org/media/course/image/72abaeb3-0856-4e89-906a-8b6eef402a1f-153a590f7f3c.small.jpg",
+    sname:"INTRODUCTION TO C++",
+    title:"Data Structures and Algorithms",
+    link:"https://www.youtube.com/embed/z9bZufPHFLU",
+},{
+    id:2,
+    imgsrc:"https://www.softwaretestinghelp.com/wp-content/qa/uploads/2019/06/autumn-lookbook.png",
+    sname:"Data Types & Type Modifiers",
+    title:"Data Structures and Algorithms",
+    link:"https://www.youtube.com/embed/cnT1oW5_ePM",
+},{
+    id:3,
+    imgsrc:"https://www.softwaretestinghelp.com/wp-content/qa/uploads/2019/05/File-Input-Output-in-C.png",
+    sname:"INPUT/OUTPUT in C++",
+    title:"Data Structures and Algorithms",
+    link:"https://www.youtube.com/embed/7dPdMtBX1d8",
+},{
+    id:4,
+    imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREFhh8Zzvlg7SC9ZYCrEHkuZw3Wnbf7o5JgA&usqp=CAU",
+    sname:"If/else Statement in C++",
+    title:"Data Structures and Algorithms",
+    link:"https://www.youtube.com/embed/cyB3HNlQyjY",
+},{
+    id:5,
+    imgsrc:"https://i.ytimg.com/vi/Y0TYYEhHvIU/hqdefault.jpg",
+    sname:"Introduction to Loops",
+    title:"Data Structures and Algorithms",
+    link:"https://www.youtube.com/embed/IlIn_D1RT7M",
+},{
+    id:6,
+    imgsrc:"https://cdn.educba.com/academy/wp-content/uploads/2019/12/Break-vs-Continue-2.jpg",
+    sname:"Break and Continue Statement",
+    title:"Data Structures and Algorithms",
+    link:"https://www.youtube.com/embed/Stf7KBiA1vs",
+},{
+    id:7,
+    imgsrc:"https://blob.sololearn.com/assets/introduction-java-cover.png",
+    sname:"Java in One shot",
+    title:"Data Structures and Algorithms",
+    link:"https://www.youtube.com/embed/UmnCZ7-9yDY",
+}
+]
+export default Vdata;
